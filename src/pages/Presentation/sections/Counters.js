150f06b113310a875c9aee98ac382f0f24de2b30
Ogechuku Chibogu <ochibogu@liveperson.com>
2025-08-01 01:24:33 +0200

    update code

--- src/pages/Presentation/sections/Counters.js
+++ src/pages/Presentation/sections/Counters.js
@@ -31,27 +31,27 @@ function Counters() {
         <Grid container item xs={12} lg={9} sx={{ mx: "auto" }}>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
-              count={70}
+              count={10}
               suffix="+"
-              title="Coded Elements"
-              description="From buttons, to inputs, navbars, alerts or cards, you are covered"
+              title="Endorsement"
+              description="Trusted by over 10 communities, reviewers, and industry voices"
             />
           </Grid>
           <Grid item xs={12} md={4} display="flex">
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, mx: 0 }} />
             <DefaultCounterCard
-              count={15}
+              count={1000000}
               suffix="+"
-              title="Design Blocks"
-              description="Mix the sections, change the colors and unleash your creativity"
+              title="Ratings"
+              description="Over a million ratings collected from real users worldwide"
             />
             <Divider orientation="vertical" sx={{ display: { xs: "none", md: "block" }, ml: 0 }} />
           </Grid>
           <Grid item xs={12} md={4}>
             <DefaultCounterCard
               count={4}
-              title="Pages"
-              description="Save 3-4 weeks of work when you use our pre-made pages for your website"
+              title="Sources"
+              description="Aggregating ratings from 4 trusted public movie databases"
             />
           </Grid>
         </Grid>
